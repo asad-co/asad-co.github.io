a695a6ae48ee389d4914e29d666514fe4a218c82
asad-co <realhk98000@gmail.com>
2024-02-05 02:51:08 +0500

    late night 5 feb

---
+++ assets/js/typed.js
@@ -0,0 +1,6 @@
+var typed = new Typed('#typedJS', {
+    strings: ['Web Developer!', 'Front-end Developer!', 'Back-end Developer!', 'MERN Stack Developer!', 'Android App Developer!', 'iOS Developer!', 'Ethical Hacker!'],
+    typeSpeed: 50,
+    shuffle: true,
+    loop: true
+});
