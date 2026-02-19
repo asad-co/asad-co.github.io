c76eac3b53a446cfac98a796ebc5b6d27684191b
asad-co <realhk98000@gmail.com>
2026-02-19 07:53:11 +0500

    [refractor] entire useless stuff removed

--- assets/js/typed.js
+++
@@ -1,6 +0,0 @@
-var typed = new Typed('#typedJS', {
-    strings: ['Web Developer!', 'Front-end Developer!', 'Back-end Developer!', 'MERN Stack Developer!', 'Android App Developer!', 'iOS Developer!', 'Ethical Hacker!'],
-    typeSpeed: 50,
-    shuffle: false,
-    loop: true
-});
